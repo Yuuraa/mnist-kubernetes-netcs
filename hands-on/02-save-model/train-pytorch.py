b5ed206364c317a2a817d3c0c4b7d9df81baca5f
Yuuraa <yoorachoi8937@gmail.com>
2021-01-14 21:56:16 +0900

    Edit to save model to mounted volume

---
+++ hands-on/02-save-model/train-pytorch.py
@@ -0,0 +1,105 @@
+import torch
+import torch.nn as nn
+import torch.nn.init
+import torch.optim as optim
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Choose the device
+isgpu = torch.cuda.is_available()
+device = torch.device('cuda' if isgpu else 'cpu')
+
+# For reproducibility
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+
+# Download & Load MNIST data
+mnist_train = dsets.MNIST(root='mnist_data/', train=True, transform=transforms.ToTensor(), download=True)
+mnist_test = dsets.MNIST(root='mnist_data/', train=False, transform=transforms.ToTensor(), download=True)
+
+data_loader = torch.utils.data.DataLoader(dataset=mnist_train, batch_size=batch_size, shuffle=True, drop_last=True)
+
+# CNN model definition
+class CNN(torch.nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.keep_prob = 0.5
+        self.layer1 = torch.nn.Sequential(
+            torch.nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer2 = torch.nn.Sequential(
+            torch.nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer3 = torch.nn.Sequential(
+            torch.nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2, padding=1)
+        )
+        self.fc1 = torch.nn.Linear(4*4*128, 625, bias=True)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+        self.layer4 = torch.nn.Sequential(
+            self.fc1,
+            torch.nn.ReLU(),
+            torch.nn.Dropout(p=1 - self.keep_prob)
+        )
+        self.fc2 = torch.nn.Linear(625, 10, bias=True)
+        torch.nn.init.xavier_uniform_(self.fc2.weight)
+    
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = out.view(out.size(0), -1) # Flatten the output for fully connected layer
+        out = self.layer4(out)
+        out = self.fc2(out)
+        return out
+
+
+# Initiate the model
+model = CNN().to(device)
+criterion = torch.nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+total_batch = len(data_loader)
+model.train()
+print('Started training')
+
+for epoch in range(training_epochs):
+    avg_cost = 0
+    for X, Y in data_loader:
+        X = X.to(device)
+        Y = Y.to(device)
+
+        optimizer.zero_grad()
+        hypothesis = model(X)
+        cost = criterion(hypothesis, Y)
+        cost.backward()
+        optimizer.step()
+
+        avg_cost += cost / total_batch
+    print('[Epoch: {:>4}] cost = {:>.9}'.format(epoch + 1, avg_cost))
+
+print('Training completed')
+
+with torch.no_grad():
+    model.eval()
+
+    X_test = mnist_test.test_data.view(len(mnist_test), 1, 28, 28).float().to(device)
+    Y_test = mnist_test.test_labels.to(device)
+
+    prediction = model(X_test)
+    correct_prediction = torch.argmax(prediction, 1) == Y_test
+    acccuarcy = correct_prediction.float().mean()
+    print('Accuracy: ', acccuarcy.item())
+
+import datetime
+torch.save(model.state_dict(), '/model_storage/model-%s.pth' % datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
